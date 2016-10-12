668d5b4c3b66e8000f92281e46a0f813f31c625b
neporood <neporood@live.com>
2016-10-12 09:40:13 -0500

    Add Bliss Flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -136,3 +136,15 @@ BOARD_SEPOLICY_UNION := \
        system.te \
        netd.te \
        file_contexts
+
+# Bliss Settings
+BLISS_O3 := true
+# BLISS_GRAPHITE := true
+# BLISS_STRICT:= true
+BLISS_PIPE := true
+TARGET_TC_ROM := 4.9
+TARGET_TC_KERNEL := 4.8
+TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := $(TARGET_TC_KERNEL)
+
+-include vendor/bliss/config/sm.mk
