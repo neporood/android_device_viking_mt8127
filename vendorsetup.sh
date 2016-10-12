157bc8b97c22d9340c07bf35519c299720eafb27
Nepo Rood <neporood@users.noreply.github.com>
2016-10-12 02:41:11 -0500

    Blissify

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,6 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo cm_RCT6303W87DK
-add_lunch_combo cm_RCT6303W87DK-eng
-add_lunch_combo cm_RCT6303W87DK-userdebug
+add_lunch_combo bliss_RCT6303W87DK-eng
+add_lunch_combo bliss_RCT6303W87DK-userdebug
